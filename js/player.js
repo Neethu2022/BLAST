a8a3b2157c8499d17004b89fe666d7bbd603c55c
Neethu Kunjappan <neethu.kunjappan@tu_clausthal.de>
2023-06-13 17:37:22 +0530

    add end game scene

--- js/player.js
+++ js/player.js
@@ -61,53 +61,53 @@ class Player {
   }
 }
 
-class player extends Component {
-  constructor(gameScreen, left, top, width, height, imgSrc) {
-    super(gameScreen, left, top, width, height, imgSrc);
+// class player extends Component {
+//   constructor(gameScreen, left, top, width, height, imgSrc) {
+//     super(gameScreen, left, top, width, height, imgSrc);
 
-    this.directionX = 0;
-    this.directionY = 0;
-  }
+//     this.directionX = 0;
+//     this.directionY = 0;
+//   }
 
-  move() {
-    // Update player's car position based on directionX and directionY
-    this.left += this.directionX;
-    this.top += this.directionY;
-    debugger;
+//   move() {
+//     // Update player's car position based on directionX and directionY
+//     this.left += this.directionX;
+//     this.top += this.directionY;
+//     debugger;
 
-    // Ensure the player's car stays within the game screen
-    if (this.left < 10) {
-      this.left = 10;
-    }
-    if (this.top < 10) {
-      this.top = 10;
-    }
-    if (this.left > this.gameScreen.offsetWidth - this.width - 10) {
-      this.left = this.gameScreen.offsetWidth - this.width - 10;
-    }
-    if (this.top > this.gameScreen.offsetHeight - this.height - 10) {
-      this.top = this.gameScreen.offsetHeight - this.height - 10;
-    }
+//     // Ensure the player's car stays within the game screen
+//     if (this.left < 10) {
+//       this.left = 10;
+//     }
+//     if (this.top < 10) {
+//       this.top = 10;
+//     }
+//     if (this.left > this.gameScreen.offsetWidth - this.width - 10) {
+//       this.left = this.gameScreen.offsetWidth - this.width - 10;
+//     }
+//     if (this.top > this.gameScreen.offsetHeight - this.height - 10) {
+//       this.top = this.gameScreen.offsetHeight - this.height - 10;
+//     }
 
-    // Update the player's car position on the screen
-    this.updatePosition();
-  }
+//     // Update the player's car position on the screen
+//     this.updatePosition();
+//   }
 
-  didCollide(obstacle) {
-    const playerRect = this.element.getBoundingClientRect();
-    const obstacleRect = obstacle.element.getBoundingClientRect();
+//   didCollide(obstacle) {
+//     const playerRect = this.element.getBoundingClientRect();
+//     const obstacleRect = obstacle.element.getBoundingClientRect();
 
-    if (
-      playerRect.left < obstacleRect.right &&
-      playerRect.right > obstacleRect.left &&
-      playerRect.top < obstacleRect.bottom &&
-      playerRect.bottom > obstacleRect.top
-    ) {
-      console.log("Crash!");
-      game.bouncesound.play();
-      return true;
-    } else {
-      return false;
-    }
-  }
-}
+//     if (
+//       playerRect.left < obstacleRect.right &&
+//       playerRect.right > obstacleRect.left &&
+//       playerRect.top < obstacleRect.bottom &&
+//       playerRect.bottom > obstacleRect.top
+//     ) {
+//       console.log("Crash!");
+//       game.bouncesound.play();
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
